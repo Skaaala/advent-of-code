ae8ebc8a06b092b32a448d56d1dddb44c486d091
Jan Skalický <skalicky.honza@gmail.com>
2022-12-02 23:54:30 +0100

    fixes

---
+++ src/2022/02/02-enums.ts
@@ -0,0 +1,23 @@
+export enum OpponentTurn {
+    ROCK = 'A',
+    PAPER = 'B',
+    SCISSORS = 'C',
+}
+
+export enum MyTurn {
+    ROCK = 'X',
+    PAPER = 'Y',
+    SCISSORS = 'Z',
+}
+
+export enum HowTurnShouldEnd {
+    LOST = 'X',
+    DRAW = 'Y',
+    WIN = 'Z',
+}
+
+export enum RoundPoints {
+    LOST = 0,
+    DRAW = 3,
+    WIN = 6,
+}
