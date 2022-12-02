1abd791520e3cdb8034678ac15cb9dd08cfcf643
Jan Skalický <skalicky.honza@gmail.com>
2022-12-03 00:25:39 +0100

    npm lib

---
+++ src/2022/index.ts
@@ -0,0 +1,3 @@
+export * from './01/01'
+export * from './02/02'
+export * from './02/02-enums'
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { BaseDay } from './2022/base/base-day';
-import { Day01 } from './2022/01/01';
-import { Day02 } from './2022/02/02';
+// import { BaseDay } from './2022/base/base-day';
+// import { Day01 } from './2022/01/01';
+// import { Day02 } from './2022/02/02';
 
-const resultOfDay = (day: BaseDay) => {
-    console.log('First part')
-    console.log(day.partOne());
-    console.log('\nSecond part');
-    console.log(day.partTwo());
-}
+// const resultOfDay = (day: BaseDay) => {
+//     console.log('First part')
+//     console.log(day.partOne());
+//     console.log('\nSecond part');
+//     console.log(day.partTwo());
+// }
+//
+// export function main() {
+//     resultOfDay(new Day01());
+//     resultOfDay(new Day02());
+// }
+//
+// main();
 
-export function main() {
-    resultOfDay(new Day01());
-    resultOfDay(new Day02());
-}
-
-main();
+export * from './2022/index'
